3adb44986a68a0f9f639c857778295b08a7d4894
bagussssyyy <bagussyaifudin45@gmail.com>
2025-07-05 19:37:15 +0700

    Add files via upload

---
+++ cs3.js
@@ -0,0 +1,33 @@
+
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Beranda - Muhammad Rama Saputra</title>
+  <link rel="stylesheet" href="style3.css">
+</head>
+<body>
+  <header>
+    <h1>Selamat Datang di Portofolio Saya</h1>
+    <nav>
+      <ul>
+       <li><a href="index3.html">Beranda</a></li>
+        <li><a href="about3.html">Tentang Saya</a></li>
+        <li><a href="portfolio3.html">Portofolio</a></li>
+        <li><a href="contact3.html">Kontak</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <section class="intro">
+      <img src="foto profil.jpeg" alt="Foto  Muhammad Rama Saputra" width="200">
+      <h2> Muhammad Rama Saputra</h2>
+      <p>Mahasiswa Sistem Informasi, Universitas Islam Nahdlatul Ulama Jepara</p>
+      <blockquote>"Menuntut ilmu setinggi langit"</blockquote>
+    </section>
+  </main>
+  <footer>
+    <p>&copy; 2025  Muhammad Rama Saputra</p>
+  </footer>
+</body>
+</html>
